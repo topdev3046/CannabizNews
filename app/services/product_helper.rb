cc14152e794739daeaf9bcc0c743d63eaa17fdd3
EC2 Default User <ec2-user@ip-172-31-36-223.ec2.internal>
2018-06-08 16:06:44 +0000

    product table fixes

--- app/services/product_helper.rb
+++ app/services/product_helper.rb
@@ -5,73 +5,72 @@ class ProductHelper
 		@state = state
 	end
 	
-# 	def findProductsPriceAndDistance()
-		
-# 		#hash returned
-# 		@product_to_distance = Hash.new
-# 		@product_to_closest_disp = Hash.new
-		
-# 		@products.each do |product|
+	# def findProductsPriceAndDistance()		
+	# 	#hash returned
+	# 	@product_to_distance = Hash.new
+	# 	@product_to_closest_disp = Hash.new
+ #    	@products.each do |product|
 			
-# 			#distance
-# 			if @ip_address != nil
-# 				closest_distance = nil
-# 				closest_dispensary = nil
+	# 		#distance
+	# 		if @ip_address != nil
+	# 			closest_distance = nil
+	# 			closest_dispensary = nil
 				
-# 				product.dispensary_sources.each do |dispSource|
+	# 			product.dispensary_sources.each do |dispSource|
 					
-# 					if closest_distance == nil || dispSource.distance_to(@ip_address) < closest_distance
-# 						closest_distance = dispSource.distance_to(@ip_address).round(2)	
-# 						closest_dispensary = dispSource.dispensary
-# 					end
-# 				end
+	# 				if closest_distance == nil || dispSource.distance_to(@ip_address) < closest_distance
+	# 					closest_distance = dispSource.distance_to(@ip_address).round(2)	
+	# 					closest_dispensary = dispSource.dispensary
+	# 				end
+	# 			end
 				
-# 				@product_to_distance.store(product, closest_distance)
-# 				@product_to_closest_disp.store(product, closest_dispensary)
-# 			end
-# 		end
+	# 			@product_to_distance.store(product, closest_distance)
+	# 			@product_to_closest_disp.store(product, closest_dispensary)
+	# 		end
+	# 	end
 		
-# 		#return
-# 		[@product_to_distance, @product_to_closest_disp]
-# 	end
+	# 	#return
+	# 	[@product_to_distance, @product_to_closest_disp]
+	# end
 	
 	def buildProductDisplay()
+		
+        #similar products - include is_dom and sub_category as well
+        if @product.category.present?
 
-	    #similar products - include is_dom and sub_category as well
-	    if @product.category.present?
-
-	        @similar_products = @product.category.products.featured.where.not(id: @product.id)
-	        
-	        if @product.is_dom.present?
-	        
-	            @similar_products = @similar_products.where(is_dom: @product.is_dom).order("Random()").limit(4)
-	            
-	        elsif @product.sub_category.present?
-	        
-	            @similar_products = @similar_products.where(sub_category: @product.sub_category).order("Random()").limit(4)
-	        else
-	            @similar_products = @similar_products.order("Random()").limit(4)    
-	        end
-	        
-	    else
+            @similar_products = @product.category.products.featured.where.not(id: @product.id)
+            
+            if @product.is_dom.present?
+            
+                @similar_products = @similar_products.where(is_dom: @product.is_dom).order("Random()").limit(4)
+                
+            elsif @product.sub_category.present?
+            
+                @similar_products = @similar_products.where(sub_category: @product.sub_category).order("Random()").limit(4)
+            else
+                @similar_products = @similar_products.order("Random()").limit(4)    
+            end
+            
+        else
             @similar_products = Product.featured.order("Random()").limit(4)  
-	    end
-	    
-	    dispensary_sources = @product.dispensary_sources.where(state_id: @state.id).
-                        includes(:dispensary, :state, :dispensary_source_products => :dsp_prices).
-                        order('last_menu_update DESC').order("name ASC")
+        end
         
-        #table headers
-        # header_options = nil
-        header_options =  @product.dispensary_source_products.map{|dispensary_source| dispensary_source.dsp_prices.map(&:unit)}.flatten.uniq unless  @product.dispensary_source_products.blank?
-        if header_options != nil
-            @table_header_options = DspPrice::DISPLAYS.sort_by {|key, value| value}.to_h.select{|k, v| k if header_options.include?(k)}.keys
+        
+        #populate page maps - IF THEY HAVE A SELF ONE THEN AUTOMATICALLY USE THAT, IF NOT USE ANOTHER
+        dispensary_sources = @product.dispensary_sources.where(state_id: @state.id).
+                                includes(:dispensary, :state, :dispensary_source_products => :dsp_prices).
+                                order('last_menu_update DESC').order("name ASC")
+                                
+        #need a map of dispensary to dispensary source product
+        @dispensary_to_product = Hash.new
+        dispensary_source_products = @product.dispensary_source_products.includes(:product, :dispensary_source, :dsp_prices).where("dispensary_sources.state_id =?", @state.id).references(:dispensary_sources)
+        @header_options =  dispensary_source_products.map{|dispensary_source_product| dispensary_source_product.dsp_prices.map(&:unit)}.flatten.uniq  unless  dispensary_source_products.blank?
+        if @header_options != nil
+            @table_header_options = DspPrice::DISPLAYS.sort_by {|key, value| value}.to_h.select{|k, v| k if @header_options.include?(k)}.keys  unless @header_options.blank?
         else 
             @table_header_options = nil
         end
         
-        #need a map of dispensary to dispensary source product
-        @dispensary_to_product = Hash.new
         dispensary_sources.each do |dispSource|
             
             #dispensary products
@@ -85,8 +84,7 @@ class ProductHelper
             end
         end
         
+        #return
         [@similar_products, @dispensary_to_product, @table_header_options]
-		
 	end
-	
 end
