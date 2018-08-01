bb820da142990931ca03d0c2ffad28640cffc0c0
EC2 Default User <ec2-user@ip-172-31-36-223.ec2.internal>
2018-08-01 18:53:50 +0000

    update of sitemap

--- app/controllers/sitemaps_controller.rb
+++ app/controllers/sitemaps_controller.rb
@@ -2,10 +2,11 @@ class SitemapsController < ApplicationController
   def index
     @static_pages = [root_url]
     @products = Product.featured
+    @blogs = Blog.published_blogs.order("published_date DESC")
     @vendors = Vendor.all
     @dispensaries = Dispensary.all
     @articles = Article.all
-    @blogs = Blog.published.order("published_date DESC")
+    
 
     respond_to do |format|
       format.xml
