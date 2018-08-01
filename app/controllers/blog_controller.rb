a3f42adee199e85754fcd2d0d3159df8ae1dd480
EC2 Default User <ec2-user@ip-172-31-36-223.ec2.internal>
2018-08-01 16:12:15 +0000

    blog error messages

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -3,41 +3,48 @@ class BlogController < ApplicationController
     
     def show
         
-        # go back if not published
-        if @blog.published
-            
-            #site visitor state
-            if params[:state].present?
-                if state = State.find_by(name: params[:state])
-                    @site_visitor_state = state
-                end
-            end
+        begin
         
-            #product sidebar
-            if @site_visitor_state.present? && @site_visitor_state.product_state
+            # go back if not published
+            if @blog.published
                 
-                if Rails.env.production? 
-                    @top_products = @site_visitor_state.products.featured.joins(:dispensary_source_products).group("products.id").having("count(dispensary_source_products.id)>4").
-                                        includes(:vendors, :category, :average_prices).
-                                        order("RANDOM()").limit(3)
-                else
-                    @top_products = @site_visitor_state.products.featured.includes(:vendors, :category, :average_prices).
-                                        order("RANDOM()").limit(3)
+                #site visitor state
+                if params[:state].present?
+                    if state = State.find_by(name: params[:state])
+                        @site_visitor_state = state
+                    end
                 end
-            else 
-                @top_products = Product.featured.joins(:dispensary_source_products).group("products.id").having("count(dispensary_source_products.id)>4").
-                                        includes(:vendors, :category, :average_prices).
-                                        order("RANDOM()").limit(3)
-            end
-    
-            @other_blog_posts = Blog.where.not(id: @blog.id).order("RANDOM()").limit(3)
             
-            #add view to post for sorting
-            @blog.increment(:num_views, by = 1)
-            @blog.save
-        else
-            redirect_to root_path
-        end
+                #product sidebar
+                if @site_visitor_state.present? && @site_visitor_state.product_state
+                    
+                    if Rails.env.production? 
+                        @top_products = @site_visitor_state.products.featured.joins(:dispensary_source_products).group("products.id").having("count(dispensary_source_products.id)>4").
+                                            includes(:vendors, :category, :average_prices).
+                                            order("RANDOM()").limit(3)
+                    else
+                        @top_products = @site_visitor_state.products.featured.includes(:vendors, :category, :average_prices).
+                                            order("RANDOM()").limit(3)
+                    end
+                else 
+                    @top_products = Product.featured.joins(:dispensary_source_products).group("products.id").having("count(dispensary_source_products.id)>4").
+                                            includes(:vendors, :category, :average_prices).
+                                            order("RANDOM()").limit(3)
+                end
+        
+                @other_blog_posts = Blog.where.not(id: @blog.id).order("RANDOM()").limit(3)
+                
+                #add view to post for sorting
+                @blog.increment(:num_views, by = 1)
+                @blog.save
+            else
+                redirect_to root_path
+            end
+        rescue => ex
+            puts 'HERE IS THE ERROR: '
+            puts ex
+            ErrorFound.email("Blog Show for: #{@blog.slug}" , ex.inspect, ex.message, ex.backtrace.join("\n")).deliver_now   
+        end 
         
     end
     
