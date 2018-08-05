d2c5f4e2e4d81b2ec974ee219494b85f69740dcb
EC2 Default User <ec2-user@ip-172-31-36-223.ec2.internal>
2018-08-05 12:56:34 +0000

    update cron schedule

--- config/initializers/sidekiq_cron.rb
+++ config/initializers/sidekiq_cron.rb
@@ -1,49 +1,49 @@
 if Rails.env.production?
 	
-	# NEWS
-	Sidekiq::Cron::Job.create(name: 'News Leafly', cron: '10 2,6,10,14,18,22 * * *', class: 'LeaflyWorker')
-	Sidekiq::Cron::Job.create(name: 'News CannaLawBlog', cron: '15 2,6,10,14,18,22 * * *', class: 'CannaLawBlogWorker')
-	Sidekiq::Cron::Job.create(name: 'News CannabisCulture', cron: '20 2,6,10,14,18,22 * * *', class: 'CannabisCultureWorker')
-	# Sidekiq::Cron::Job.create(name: 'DopeMagazine News', cron: '25 2,6,10,14,18,22 * * *', class: 'DopeMagazineWorker')
-	# Sidekiq::Cron::Job.create(name: 'FourTwentyTimes News', cron: '30 2,6,10,14,18,22 * * *', class: 'FourTwentyTimesWorker')
-	Sidekiq::Cron::Job.create(name: 'News HighTimes', cron: '35 2,6,10,14,18,22 * * *', class: 'HighTimesWorker')
-	Sidekiq::Cron::Job.create(name: 'News MarijuanaStocks', cron: '40 2,6,10,14,18,22 * * *', class: 'MarijuanaStocksWorker')
-	Sidekiq::Cron::Job.create(name: 'News Marijuana.com', cron: '45 2,6,10,14,18,22 * * *', class: 'MarijuanaWorker')
-	Sidekiq::Cron::Job.create(name: 'News MjBizDaily', cron: '50 2,6,10,14,18,22 * * *', class: 'MjBizDailyWorker')
-	Sidekiq::Cron::Job.create(name: 'News TheCannabist', cron: '55 2,6,10,14,18,22 * * *', class: 'TheCannabistWorker')
+	# NEWS - runs at 8am, 1pm, 6pm daily
+	Sidekiq::Cron::Job.create(name: 'News Leafly', cron: '10 8,13,18 * * *', class: 'LeaflyWorker')
+	Sidekiq::Cron::Job.create(name: 'News CannaLawBlog', cron: '15 8,13,18 * * *', class: 'CannaLawBlogWorker')
+	Sidekiq::Cron::Job.create(name: 'News CannabisCulture', cron: '20 8,13,18 * * *', class: 'CannabisCultureWorker')
+	# Sidekiq::Cron::Job.create(name: 'DopeMagazine News', cron: '25 8,13,18 * * *', class: 'DopeMagazineWorker')
+	# Sidekiq::Cron::Job.create(name: 'FourTwentyTimes News', cron: '30 8,13,18 * * *', class: 'FourTwentyTimesWorker')
+	Sidekiq::Cron::Job.create(name: 'News HighTimes', cron: '35 8,13,18 * * *', class: 'HighTimesWorker')
+	Sidekiq::Cron::Job.create(name: 'News MarijuanaStocks', cron: '40 8,13,18 * * *', class: 'MarijuanaStocksWorker')
+	Sidekiq::Cron::Job.create(name: 'News Marijuana.com', cron: '45 8,13,18 * * *', class: 'MarijuanaWorker')
+	Sidekiq::Cron::Job.create(name: 'News MjBizDaily', cron: '50 8,13,18 * * *', class: 'MjBizDailyWorker')
+	Sidekiq::Cron::Job.create(name: 'News TheCannabist', cron: '55 8,13,18 * * *', class: 'TheCannabistWorker')
 	
-	# LEAFLY
+	# LEAFLY - runs at 1 am, 2am
 	Sidekiq::Cron::Job.create(name: 'Leafly Disp WA', cron: '0 1 * * *', class: 'LeaflyDispensaryWorker1')
-	Sidekiq::Cron::Job.create(name: 'Leafly Disp CO', cron: '20 1 * * *', class: 'LeaflyDispensaryWorker2')
-	Sidekiq::Cron::Job.create(name: 'Leafly Disp NV', cron: '40 1 * * *', class: 'LeaflyDispensaryWorker3')
+	Sidekiq::Cron::Job.create(name: 'Leafly Disp CO', cron: '0 2 * * *', class: 'LeaflyDispensaryWorker2')
+	Sidekiq::Cron::Job.create(name: 'Leafly Disp NV', cron: '0 3 * * *', class: 'LeaflyDispensaryWorker3')
 	
-	#POTGUIDE
-	Sidekiq::Cron::Job.create(name: 'Potguide Disp WA 1', cron: '0 3 * * *', class: 'PotguideWorker1')
-	Sidekiq::Cron::Job.create(name: 'Potguide Disp WA 2', cron: '30 3 * * *', class: 'PotguideWorker2')
-	Sidekiq::Cron::Job.create(name: 'Potguide Disp WA 3', cron: '0 4 * * *', class: 'PotguideWorker3')
-	Sidekiq::Cron::Job.create(name: 'Potguide Disp WA 4', cron: '30 4 * * *', class: 'PotguideWorker4')
-	
-	Sidekiq::Cron::Job.create(name: 'Potguide Disp CO 5', cron: '0 5 * * *', class: 'PotguideWorker5')
-	Sidekiq::Cron::Job.create(name: 'Potguide Disp CO 6', cron: '15 5 * * *', class: 'PotguideWorker6')
-	Sidekiq::Cron::Job.create(name: 'Potguide Disp CO 7', cron: '30 5 * * *', class: 'PotguideWorker7')
-	Sidekiq::Cron::Job.create(name: 'Potguide Disp CO 8', cron: '45 5 * * *', class: 'PotguideWorker8')
+	#POTGUIDE - runs at 4am - 7am, takes 8am break for news, runs 9am - 12pm
+	Sidekiq::Cron::Job.create(name: 'Potguide Disp CO 1', cron: '0 4 * * *', class: 'PotguideWorker5')
+	Sidekiq::Cron::Job.create(name: 'Potguide Disp CO 2', cron: '0 5 * * *', class: 'PotguideWorker6')
+	Sidekiq::Cron::Job.create(name: 'Potguide Disp CO 3', cron: '0 6 * * *', class: 'PotguideWorker7')
+	Sidekiq::Cron::Job.create(name: 'Potguide Disp CO 4', cron: '0 7 * * *', class: 'PotguideWorker8')
 
-	# WEED MAPS
-	Sidekiq::Cron::Job.create(name: 'Weedmaps Disp WA 1', cron: '0 7 * * *', class: 'WeedMapsWorker1')
-	Sidekiq::Cron::Job.create(name: 'Weedmaps Disp WA 2', cron: '15 7 * * *', class: 'WeedMapsWorker2')
-	Sidekiq::Cron::Job.create(name: 'Weedmaps Disp WA 3', cron: '30 7 * * *', class: 'WeedMapsWorker3')
-	Sidekiq::Cron::Job.create(name: 'Weedmaps Disp WA 4', cron: '45 7 * * *', class: 'WeedMapsWorker4')
-	
-	Sidekiq::Cron::Job.create(name: 'Weedmaps Disp CO 5', cron: '0 9 * * *', class: 'WeedMapsWorker5')
-	Sidekiq::Cron::Job.create(name: 'Weedmaps Disp CO 6', cron: '15 9 * * *', class: 'WeedMapsWorker6')
-	Sidekiq::Cron::Job.create(name: 'Weedmaps Disp CO 7', cron: '30 9 * * *', class: 'WeedMapsWorker7')
-	Sidekiq::Cron::Job.create(name: 'Weedmaps Disp CO 8', cron: '45 9 * * *', class: 'WeedMapsWorker8')
-	
-	Sidekiq::Cron::Job.create(name: 'Weedmaps Disp NV 9', cron: '0 11 * * *', class: 'WeedMapsWorker9')
-	Sidekiq::Cron::Job.create(name: 'Weedmaps Disp NV 10', cron: '15 11 * * *', class: 'WeedMapsWorker10')
-	Sidekiq::Cron::Job.create(name: 'Weedmaps Disp NV 11', cron: '30 11 * * *', class: 'WeedMapsWorker11')
-	Sidekiq::Cron::Job.create(name: 'Weedmaps Disp NV 12', cron: '45 11 * * *', class: 'WeedMapsWorker12')
+	Sidekiq::Cron::Job.create(name: 'Potguide Disp WA 1', cron: '0 9 * * *', class: 'PotguideWorker1')
+	Sidekiq::Cron::Job.create(name: 'Potguide Disp WA 2', cron: '0 10 * * *', class: 'PotguideWorker2')
+	Sidekiq::Cron::Job.create(name: 'Potguide Disp WA 3', cron: '0 11 * * *', class: 'PotguideWorker3')
+	Sidekiq::Cron::Job.create(name: 'Potguide Disp WA 4', cron: '0 12 * * *', class: 'PotguideWorker4')
 
-	# HEADSET
-	Sidekiq::Cron::Job.create(name: 'Headset Scraper', cron: '0 0 * * *', class: 'HeadsetWorker')
+	# WEED MAPS - runs every 45 mins from 2pm - midnight with a break at 6pm for news
+	Sidekiq::Cron::Job.create(name: 'Weedmaps Disp CO 1', cron: '0 14 * * *', class: 'WeedMapsWorker5')
+	Sidekiq::Cron::Job.create(name: 'Weedmaps Disp CO 2', cron: '45 14 * * *', class: 'WeedMapsWorker6')
+	Sidekiq::Cron::Job.create(name: 'Weedmaps Disp CO 3', cron: '30 15 * * *', class: 'WeedMapsWorker7')
+	Sidekiq::Cron::Job.create(name: 'Weedmaps Disp CO 4', cron: '15 16 * * *', class: 'WeedMapsWorker8')
+	
+	Sidekiq::Cron::Job.create(name: 'Weedmaps Disp NV 1', cron: '0 17 * * *', class: 'WeedMapsWorker9')
+	Sidekiq::Cron::Job.create(name: 'Weedmaps Disp NV 2', cron: '0 19 * * *', class: 'WeedMapsWorker10')
+	Sidekiq::Cron::Job.create(name: 'Weedmaps Disp NV 3', cron: '45 19 * * *', class: 'WeedMapsWorker11')
+	Sidekiq::Cron::Job.create(name: 'Weedmaps Disp NV 4', cron: '30 20 * * *', class: 'WeedMapsWorker12')
+
+	Sidekiq::Cron::Job.create(name: 'Weedmaps Disp WA 1', cron: '15 21 * * *', class: 'WeedMapsWorker1')
+	Sidekiq::Cron::Job.create(name: 'Weedmaps Disp WA 2', cron: '00 22 * * *', class: 'WeedMapsWorker2')
+	Sidekiq::Cron::Job.create(name: 'Weedmaps Disp WA 3', cron: '45 22 * * *', class: 'WeedMapsWorker3')
+	Sidekiq::Cron::Job.create(name: 'Weedmaps Disp WA 4', cron: '30 23 * * *', class: 'WeedMapsWorker4')
+
+	# HEADSET - runs at 12:30am daily
+	Sidekiq::Cron::Job.create(name: 'Headset Scraper', cron: '30 0 * * *', class: 'HeadsetWorker')
 end
