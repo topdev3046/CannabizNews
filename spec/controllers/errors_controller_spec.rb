b4395eb8958370c866a5f2b3d101ef49a64c1fdd
EC2 Default User <ec2-user@ip-172-31-36-223.ec2.internal>
2018-08-20 13:54:40 +0000

    commenting out all old test classes to start fresh

--- spec/controllers/errors_controller_spec.rb
+++ spec/controllers/errors_controller_spec.rb
@@ -1,21 +1,21 @@
 # frozen_string_literal: true
 
-require "rails_helper"
+# require "rails_helper"
 
-RSpec.describe ErrorsController, type: :controller do
+# RSpec.describe ErrorsController, type: :controller do
 
-  describe "GET #not_found" do
-    it "returns http success" do
-      get :not_found
-      expect(response).to have_http_status(:success)
-    end
-  end
+#   describe "GET #not_found" do
+#     it "returns http success" do
+#       get :not_found
+#       expect(response).to have_http_status(:success)
+#     end
+#   end
 
-  describe "GET #internal_server_error" do
-    it "returns http success" do
-      get :internal_server_error
-      expect(response).to have_http_status(:success)
-    end
-  end
+#   describe "GET #internal_server_error" do
+#     it "returns http success" do
+#       get :internal_server_error
+#       expect(response).to have_http_status(:success)
+#     end
+#   end
 
-end
+# end
